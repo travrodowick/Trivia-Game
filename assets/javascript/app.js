d79986d55c6fe80870af6ed67b3444401b77d9ac
travis rodowick <travrodowick@yahoo.com>
2019-07-17 21:09:18 -0700

    added for loop function for displaying questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,66 +1,87 @@
-document.ready
+$(document).ready(function () {
 
-//========================================GLOBAL VARIABLES=====================
-var score = 0
-
- //objects with keys as answers
-var questions = [{
-    question:  "how much wood could a woodchuck chuck?",
-    imgSrc:  "",
-    A:  "32 trees worth",
-    B:  "about as much wood a woodchuck could",
-    C:  "woodchucks are extinct",
-    D:  "not as much as a lumber jack"
-    },{
-    question2:  "how any times have crewed space shuttles landed on the moon?",
-    imgSrc:  "",
-    A:  "never. unless you count in Hollywood",
-    B:  "1 time",
-    C:  "6 times",
-    D:  "2 times",
-    E:  "the earth is flat"
-    }
-];
+    //========================================GLOBAL VARIABLES=====================
+    var score = 0
+    timeLeft = 5
 
 
-timeLeft = 5
+    //objects with keys as answers
+    var questions = [{
+        question: "how much wood could a woodchuck chuck?",
+        imgSrc: "",
+        answers: ["32 trees worth", "about as much wood a woodchuck could", "woodchucks are extinct", "not as much as a lumber jack"],
+        correct: "about as much wood a woodchuck could"
+    }, {
+        question: "how any times have crewed space shuttles landed on the moon?",
+        imgSrc: "",
+        answers: ["never. unless you count in Hollywood", "1 time", "2 times", "6 times", "the earth is flat"],
+        correct: '6 times'
+    }, {
+        question: "whats the answer to question 3?",
+        imgSrc: "",
+        answers: ["A...first choice", "B", "C is the answer most of the time", "unless D is the answer and C's just there to trick you"],
+        correct: "C is the answer most of the time"
+    }];
 
-//============================FUNCTIONS=========================================
 
-//---render questions function----
-function renderQuestions() {
-    var q = questions [];
 
-//-------TIMER----------
-function countDown () {
-    var timer = setInterval(function(){
-            timeLeft -- 
-            $('#timer').text(timeLeft)
-        if (timeLeft == -1 ) {
-            $('#timer').html("gotta be faster than that!");
-            clearInterval(timer)
+
+    //============================FUNCTIONS=========================================
+
+
+
+
+
+    function renderQuestions() {
+        for (var i = 0; i < questions.length; i++) {
+            $('#questions').text("heres questions:  ", questions[i].question)
+            for (var j = 0; j < questions[i].answers.length; j++) {
+
+                $('#answers').text(questions[i].answers[j])
+            }
         }
-        },  1000)
+    }
+    //-------TIMER----------
+    function countDown() {
+        var timer = setInterval(function () {
+            timeLeft--
+            $('#timer').text(timeLeft)
+            if (timeLeft == -1) {
+                $('#timer').html("gotta be faster than that!");
+                clearInterval(timer)
+            }
+        }, 1000)
     }
 
 
-function gameStart () {
-    $('#directions, #start').css('display', 'none')
+    function gameStart() {
+        $('#directions, #start').css('display', 'none')
 
-}
-
-//======================ON CLICK START GAME=====================================
-
-$('button').click(function(){
-    gameStart();
-    countDown();
-});
+    }
 
 
 
-// loop through questions
-for (var i =0; i < questions.length; i++) {
-    var response = //on click selection object--- key
-    if (response === questions[i].answer){
-        score++
-    }}
+
+    //=======================TESTING & DEBUGGING====================
+
+    renderQuestions()
+
+
+
+    //======================ON CLICK START GAME=====================================
+
+    $('button').click(function () {
+        gameStart();
+        countDown();
+        renderQuestions();
+    });
+
+
+
+    // loop through questions
+    // for (var i =0; i < questions.length; i++) {
+    //     var response = //on click selection object--- key
+    //     } else if (response === questions[i].answer){
+    //         score++
+    //     }
+})
